aa3ffe874e89853c7395af6b87664caa814448a3
Noor Taquee <noortaquee001@gmail.com>
2026-05-22 21:57:48 +0530

    added icon file & theme attribute

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import { createElement } from "./utils/create-dom.js";
 import { navBar } from "./components/navigation/navigation.js";
 
 export const app = document.getElementById("app") as HTMLDivElement;
+
+// MARK: theme
 app.dataset.theme = "light";
 
 export const panelContainer = createElement("div", {

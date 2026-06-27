7e54b5a268f7e75615e7b73a39d97df57ea50f54
Noor-Taquee <noortaquee001@gmail.com>
2026-06-27 18:59:32 +0530

    Refactor app structure: move CSS to app.css, update imports, and clean up index.css

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import "./styles/animations.css";
 
 import "./router.js";
 
+import { navBar } from "./components/navigation/script.js";
+
+app.appendChild(navBar);
+
 import { examplePanel } from "./pages/example-panel/page.js";
 import { homePanel } from "./pages/home-panel/page.js";
 import { settingsPanel } from "./pages/settings-panel/page.js";
+import { app } from "./app.js";
 
 homePanel.dataset.index = "0";
 examplePanel.dataset.index = "1";
